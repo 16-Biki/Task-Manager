fa2dafbc9232f54deaba047781cf5f4ecf3e4b0d
16-Biki <mahadarbikram@gmail.com>
2025-09-01 12:50:44 +0530

    final submit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-manager-ugo7.onrender.com",
+  baseURL: "https://task-manager-ugo7.onrender.com/api",
 });
 
 // Attach token to each request
